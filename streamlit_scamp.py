3ef111cded4dcbb7c7cbed98fb9f93c77a2afeb6
devrajendra <devrajendra@gmail.com>
2023-06-22 05:23:46 -0700

    Update streamlit_scamp.py

--- streamlit_scamp.py
+++ streamlit_scamp.py
@@ -1,5 +1,4 @@
 from scamp import *
-#import clockblocks
 import streamlit as st
 
 st.set_page_config(page_title = "streamlit and scamp", page_icon = ":tdata:")
@@ -9,16 +8,8 @@ st.title("streamlit and scamp")
 s = Session()
 s.tempo = 120
 
-#s.print_default_soundfont_presets()
 piano = s.new_part("piano")
-
-# 60 = c4; 61 = c4#, and 59 = b3
-
-pitch_list = [60, 64, 66]
-
 c2 = [36,38,40,41,43,41,40,38,36]
-
-#play all notes in the 18 arrays above
 
 for note in c2:
     piano.play_note(note, 0.8, 1)
